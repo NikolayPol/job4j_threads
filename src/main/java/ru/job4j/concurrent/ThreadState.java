6b4e156ad4f64673eccae844170cc5fbec45398d
NikolayPolegaev <N5445@yandex.ru>
2021-08-29 23:35:20 +0300

    1.1. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Runnable r = () -> {
+        };
+        var first = new Thread(r);
+        var second = new Thread(r);
+        System.out.println("Первый поток: " + first.getName()
+                + " Состояние: " + first.getState());
+        System.out.println("Второй поток: " + second.getName()
+                + " Состояние: " + second.getState());
+        first.start();
+        second.start();
+        while (true) {
+            if (first.getState() == Thread.State.TERMINATED
+                    && second.getState() == Thread.State.TERMINATED) {
+                System.out.println("Работа завершена");
+                break;
+            }
+        }
+    }
+}
