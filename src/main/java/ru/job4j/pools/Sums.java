f9939325543e04c6555a70d368cc9882d08a5675
NikolayPolegaev <N5445@yandex.ru>
2021-09-27 19:20:50 +0300

    4. CompletableFuture [#361626 #202416]

---
+++ src/main/java/ru/job4j/pools/Sums.java
@@ -0,0 +1,33 @@
+package ru.job4j.pools;
+
+/**
+ * Класс Sums.
+ *
+ * @author Nikolay Polegaev
+ * @version 1.0 27.09.2021
+ */
+public class Sums {
+    private int rowSum;
+    private int colSum;
+
+    public Sums(int rowSum, int colSum) {
+        this.rowSum = rowSum;
+        this.colSum = colSum;
+    }
+
+    public int getRowSum() {
+        return rowSum;
+    }
+
+    public void setRowSum(int rowSum) {
+        this.rowSum = rowSum;
+    }
+
+    public int getColSum() {
+        return colSum;
+    }
+
+    public void setColSum(int colSum) {
+        this.colSum = colSum;
+    }
+}
