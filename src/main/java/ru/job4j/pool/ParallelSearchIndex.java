12891b7cd460febcb977c42696b429ce1872a423
NikolayPolegaev <N5445@yandex.ru>
2021-10-02 16:00:03 +0300

    3. ForkJoinPool [#315067 #201637]

---
+++ src/main/java/ru/job4j/pool/ParallelSearchIndex.java
@@ -0,0 +1,64 @@
+package ru.job4j.pool;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * Класс реализует параллельный поиск индекса в массиве.
+ * См. habr.com/en/post/565924/
+ * www.youtube.com/watch?v=fGuvosd-L98
+ * @author Nikolay Polegaev
+ * @version 3.0 02.10.2021
+ */
+public class ParallelSearchIndex<T> extends RecursiveTask<Integer> {
+    private static final int THRESHOLD = Runtime.getRuntime().availableProcessors();
+    private final T[] array;
+    private final T element;
+    private final int start;
+    private final int end;
+
+    public ParallelSearchIndex(T[] array, T element, int start, int end) {
+        this.array = array;
+        this.element = element;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Integer compute() {
+        if ((end - start) <= array.length / THRESHOLD) {
+            return indexOf();
+        }
+        return ForkJoinTask.invokeAll(createTasks())
+                .stream()
+                .mapToInt(ForkJoinTask::join)
+                .max()
+                .orElse(-1);
+    }
+
+    private Collection<ParallelSearchIndex<T>> createTasks() {
+        List<ParallelSearchIndex<T>> tasks = new ArrayList<>();
+        int middle = (start + end) / 2;
+        tasks.add(new ParallelSearchIndex<>(array, element, start, middle));
+        tasks.add(new ParallelSearchIndex<>(array, element, middle + 1, end));
+        return tasks;
+    }
+
+    private int indexOf() {
+        for (int i = start; i <= end; i++) {
+            if (array[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static <T> int indexOf(T[] array, T el) {
+        ForkJoinPool pool = new ForkJoinPool();
+        return pool.invoke(new ParallelSearchIndex<>(array, el, 0, array.length - 1));
+    }
+}
---
+++ src/test/java/ru/job4j/pool/ParallelSearchIndexTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.pool;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Класс ArraySearchIndexTest тестирует класс ArraySearchIndex
+ *
+ * @author Nikolay Polegaev
+ * @version 2.0 27.09.2021
+ */
+public class ParallelSearchIndexTest {
+
+    private Integer[] array;
+
+    private int el;
+
+    @Before
+    public void setUp() {
+        int j = 4_192;
+        int length = 10_000;
+        array = new Integer[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = 17 * i - 151;
+        }
+        el = 17 * j - 151;
+    }
+
+    @Test
+    public void whenFoundIndex() {
+        int rsl = ParallelSearchIndex.indexOf(array, el);
+        assertThat(rsl, is(4_192));
+    }
+
+
+    @Test
+    public void whenNotFoundIndex() {
+        int rsl = ParallelSearchIndex.indexOf(array, 28745);
+        assertThat(rsl, is(-1));
+    }
+}
